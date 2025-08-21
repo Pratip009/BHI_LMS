b69914c5cb5b121c9349876938981b5d930f905c
Pratip Kayal <pratipkayal1@gmail.com>
2025-08-21 13:36:41 +0530

    comit

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com", "randomuser.me"],
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      bufferutil: false,
+      "utf-8-validate": false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
